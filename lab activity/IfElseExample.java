b3cf667ef8d8356e6f7f93324d7cfca7b437df32
Badrinath Lagadapati <102716560+Badrinath03@users.noreply.github.com>
2022-04-08 10:47:12 +0530

    Add files via upload

---
+++ lab activity/IfElseExample.java
@@ -0,0 +1,17 @@
+imort java.util.scanner
+
+class IfElseExample{
+public void display(){
+
+scanner s = new scanner(system.in);
+system.out.println("enter age");
+int age = s.nextInt();
+
+if(age>=18);{
+system.out.println("you are eligible")
+}
+else{
+system.out.println("you are not eligible")
+}
+}
+}
