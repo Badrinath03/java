c5fd3744df5a6760a740f47855b0e6b1e68b9d77
Badrinath Lagadapati <102716560+Badrinath03@users.noreply.github.com>
2022-04-08 11:12:43 +0530

    Update IfElseExample.java

--- practical/IfElseExample.java
+++ practical/IfElseExample.java
@@ -1,12 +1,9 @@
 imort java.util.scanner
-
 class IfElseExample{
 public void display(){
-
 scanner s = new scanner(system.in);
 system.out.println("enter age");
 int age = s.nextInt();
-
 if(age>=18);{
 system.out.println("you are eligible")
 }
